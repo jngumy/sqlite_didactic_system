b25f2734f26bbb37348dfad5d09d80cb04bb4fd6
jngumy <juangumy15@gmail.com>
2020-12-19 14:56:02 -0300

    some changes

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,34 +1,3 @@
-/*config = {
-    locateFile: filename => 'js/sql-wasm.wasm'
-  }
-  // The `initSqlJs` function is globally provided by all of the main dist files if loaded in the browser.
-  // We must specify this locateFile function if we are loading a wasm file from anywhere other than the current html page's folder.
-
-  //https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.4.0/dist/sql-wasm.wasm
-
-  initSqlJs(config).then(function(SQL){
-    //Create the database
-    var db = new SQL.Database();
-    // Run a query without reading the results
-    db.run("CREATE TABLE test (col1, col2);");
-    // Insert two rows: (1,111) and (2,222)
-    db.run("INSERT INTO test VALUES (?,?), (?,?)", [1,111,2,222]);
-
-    // Prepare a statement
-    var stmt = db.prepare("SELECT * FROM test WHERE col1 BETWEEN $start AND $end");
-    stmt.getAsObject({$start:1, $end:1}); // {col1:1, col2:111}
-
-    // Bind new values
-    stmt.bind({$start:1, $end:2});
-    while(stmt.step()) { //
-      var row = stmt.getAsObject();
-      console.log('Here is a row: ' + JSON.stringify(row));
-    }
-  });
-
-  */
-
-
  var outputElm = document.getElementById('output');
  var errorElm = document.getElementById('error');
  var execBtn = document.getElementById("execute");
@@ -55,14 +24,10 @@ function print(text) {
 	outputElm.innerHTML = text.replace(/\n/g, '<br>');
 }
 
-
-
 function error(e) {
 	console.log(e);
     errorElm.style.height = '2em';
     errorElm.style.color = 'red';
-
-    
 	errorElm.textContent = e.message;
 }
 
@@ -92,7 +57,7 @@ function execute(commands) {
 	outputElm.textContent = "Resolviendo la consulta...";
 }
 
-// Create an HTML table
+// Create an HTML table for query results
 var tableCreate = function () {
 	function valconcat(vals, tagName) {
 		if (vals.length === 0) return '';
@@ -116,10 +81,9 @@ function noerror() {
 	errorElm.style.height = '0';
 }
 
-
 // Execute the commands when the button is clicked
 function execEditorContents() {
-	noerror()
+	noerror();
 	execute(editor.getValue() + ';');
 }
 execBtn.addEventListener("click", execEditorContents, true);
@@ -144,7 +108,7 @@ var editor = CodeMirror.fromTextArea(commandsElm, {
  function loadCRUD(){
    var doc = editor.getDoc();
    //doc.replaceSelection('holaa');
-   editor.setValue(`    DROP TABLE IF EXISTS employees;
+   editor.setValue(`    DROP TABLE IF EXISTS empleados;
     CREATE TABLE empleados( id integer,  nombre text,
                             designacion text, manager integer,
                             fecha_contratacion  date, salario  integer,
@@ -302,8 +266,6 @@ function toc(msg) {
     console.log(mensaje);
     errorElm.style.height = '2em';
     errorElm.style.color = 'green';
-
-    
 	errorElm.textContent = mensaje;
 }
 
